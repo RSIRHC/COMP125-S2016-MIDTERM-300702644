693281ebf2df0a86b0402173c8f660faf1307d35
R.SIRHC <christopher.r99@live.com>
2016-06-22 13:03:32 -0400

    Innitial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,24 @@
+/**
+ * FileName: app.js
+ 
+ * @author Christopher Ritchil 
+ * @date June 22, 2016
+ * 
+ * StudentID: 300702644
+ * website: http://comp125-s2016-midterm-300702644.azurewebsites.net
+ * @description This file is the main javascript file for the web site
+ * 
+ * version: initial version 0.1.0
+ */
+
+
+// setup your IIFE (Immediately Invoked Function Expression)
+(function () {
+"use strict";
+
+
+
+
+
+})();
+
