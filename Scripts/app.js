01b40c9d5c7209d0d12b8c093f26b61cd76be8e4
R.SIRHC <christopher.r99@live.com>
2016-06-22 14:14:01 -0400

    Added some internal comments

--- Scripts/app.js
+++ Scripts/app.js
@@ -8,7 +8,7 @@
  * website: http://comp125-s2016-midterm-300702644.azurewebsites.net
  * @description This file is the main javascript file for the web site
  * 
- * version: initial version 0.1.0
+ * version: final version 0.2.0
  */
 
 
@@ -37,7 +37,7 @@
 
     // Define paragraph array
     var paragraphs = [];
-
+    //input date to the paragraph array 
     paragraphs[0] = "Now is the time to travel to Greece. The country may be going through an economic crisis, but many travellers say that it hasn't impacted the experience of visiting. Plus, there may be some great deals. Greece has 1,400 islands, though only 230 of them are inhabited. And while everyone knows about Santorini and Mykonos, there are gorgeous lesser-known islands in Greece, too.";
     paragraphs[1] = "Folegandros is almost a Greek cliché, full of beautiful whitewashed houses with bright blue doors lining cobblestoned streets on steep cliffs. Rugged and remote, without chain hotels or package tours — not even a bank or ATM — and accessible only by ferry, the volcanic island boasts solitude in spades, secluded beaches, and incredible sunsets.";
     paragraphs[2] = "Off the mainland, this island, whose surrounding waters are a designated marine park full of seals and dolphins, is a nature lover's dream. The spectacular spot's crystal clear waters and beautiful beaches are perfect for diving, and its lush flora and dense forests ideal for hikes. Since the rich landscape is chock full of indigenous herbs, it's also known for its traditional medicine, and home of the International Academy of Homeopathy.";
